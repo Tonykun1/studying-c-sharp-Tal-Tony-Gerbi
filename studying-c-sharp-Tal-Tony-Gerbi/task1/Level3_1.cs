58b0111f63ce70c6879684c25585c53e80ee3611
TonyKun <tal96047@gmail.com>
2024-05-17 11:40:15 +0300

    Add project files.

---
+++ studying-c-sharp-Tal-Tony-Gerbi/task1/Level3_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Tal_Tony_Gerbi.task1
+{
+    internal class Level3_1
+    {
+    }
+}
