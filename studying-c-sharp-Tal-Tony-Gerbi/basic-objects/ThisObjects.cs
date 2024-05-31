b03fe275af87a4f238fcc653be1ea1db5a15a821
TonyKun <tal96047@gmail.com>
2024-05-31 13:09:28 +0300

    setter and getter and this

--- studying-c-sharp-Tal-Tony-Gerbi/basic-objects/ThisObjects.cs
+++ studying-c-sharp-Tal-Tony-Gerbi/basic-objects/ThisObjects.cs
@@ -6,44 +6,74 @@ using System.Threading.Tasks;
 
 namespace studying_c_sharp_Tal_Tony_Gerbi.basic_objects
 {
-    public  class CteateObj
+    public class CteateObj
     {
         public static void updatename(ThisObjects obj)
         {
-            obj.Name = "chatshadi";
-            obj.Description = "im 50 years old";
+            obj.Name = "oded";
+            obj.Description = "im 30 years old";
+            obj.SetID(3);
         }
         public static void demo()
         {
-            ThisObjects obj = new ThisObjects("tony", "im 25 years old");
+            ThisObjects obj = new ThisObjects("tony", "im 25 years old",1);
             obj.Display();
             ThisObjects obj2 = new ThisObjects();
             obj2.Display();
             obj.updateName();
-            Console.WriteLine("name = " + obj.Name + " |Description = " + obj.Description);
+            obj.Display();
+            obj.SetID(10);
+            obj.SetDescription("im 25 years old");
+            obj.SetName("Tonykun");
+            obj.Display();
         }
     }
     public class ThisObjects
     {
         public string Name;
         public string Description;
-
-        public ThisObjects(string Name, string Description)
+        private int id;
+        public ThisObjects(string Name, string Description, int id)
         {
             this.Name = Name;
             this.Description = Description;
+            this.id = id;
         }
-        public ThisObjects():this("nick", "im 28 years old")
+        public ThisObjects() : this("nick", "im 28 years old",2)
         {
 
         }
-        public void  updateName()
+        public string GetName()
+        {
+            return this.Name;
+        }
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public string GetDescription()
+        {
+            return this.Description;
+        }
+        public void SetDescription(string Description)
+        {
+            this.Description = Description;
+        }
+        public int GetID()
+        {
+            return this.id;
+        }
+        public void SetID(int id)
+        {
+            this.id = id;
+        }
+        public void updateName()
         {
             CteateObj.updatename(this);
         }
-        public  void Display()
+        public void Display()
         {
-            Console.WriteLine("name = "+ this.Name + " |Description = " + this.Description);
+            Console.WriteLine("name = " + this.Name + " |Description = " + this.Description + " |id = " + this.id);
         }
     }
 }
