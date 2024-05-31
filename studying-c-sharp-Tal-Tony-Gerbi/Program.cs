c5fc644c52939b2820a9b8105996ac3c35ad6848
TonyKun <tal96047@gmail.com>
2024-05-31 18:06:05 +0300

    demo Stock

--- studying-c-sharp-Tal-Tony-Gerbi/Program.cs
+++ studying-c-sharp-Tal-Tony-Gerbi/Program.cs
@@ -1,10 +1,13 @@
 ﻿using studying_c_sharp_Tal_Tony_Gerbi.task2;
 using studying_c_sharp_Tal_Tony_Gerbi.basic_objects;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         // Level1.RunMe();
-        TestBasicObject.Demo();
+        // TestBasicObject.Demo();
+        ExchangeDemoHomeWork Demo= new ExchangeDemoHomeWork();
+        Demo.Demo();
     }
 }
