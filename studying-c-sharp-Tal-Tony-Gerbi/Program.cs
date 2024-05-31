edcae107809f3ddce8c8c16ec9f7d79dd2467176
TonyKun <tal96047@gmail.com>
2024-05-31 10:23:29 +0300

    save old conflict

--- studying-c-sharp-Tal-Tony-Gerbi/Program.cs
+++ studying-c-sharp-Tal-Tony-Gerbi/Program.cs
@@ -6,6 +6,6 @@ public class Program
     private static void Main(string[] args)
     {
         Level1.RunMe();
-        conflictFile.LifeConflict2();
+        conflictFile.LifeConflict();
     }
 }
