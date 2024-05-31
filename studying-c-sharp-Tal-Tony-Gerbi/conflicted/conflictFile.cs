547912a654465c51234ff51c7ebbf76d2a538bb4
TonyKun <tal96047@gmail.com>
2024-05-31 10:19:54 +0300

    save old conflict

--- studying-c-sharp-Tal-Tony-Gerbi/conflicted/conflictFile.cs
+++ studying-c-sharp-Tal-Tony-Gerbi/conflicted/conflictFile.cs
@@ -8,9 +8,9 @@ namespace studying_c_sharp_Tal_Tony_Gerbi.conflicted
 {
     internal class conflictFile
     {
-        public static void LifeConflict1()
+        public static void LifeConflict2()
         {
-            Console.WriteLine("this is new Conflict");
+            Console.WriteLine("this is old Conflict");
             Console.WriteLine("TEST  at CONFLICT");
         }
     }
